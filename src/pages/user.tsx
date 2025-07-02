5bc48a5233eb12f847b8016741cd312b2974c275
unknown <chenlong26@cn.net.ntes>
2025-07-02 09:59:41 +0800

    add user componrnt

---
+++ src/pages/user.tsx
@@ -0,0 +1,15 @@
+const User= ()=>{
+    return (
+        <div>
+            <p>
+                this is a pro
+            </p>
+
+            <ul>
+                <li>
+                    111
+                </li>
+            </ul>
+        </div>
+    )
+}
